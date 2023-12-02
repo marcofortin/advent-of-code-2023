542e68582fbf98e1917ca7a131218e98901ea910
marcofortin <mfortin.uwaterloo@gmail.com>
2023-12-02 12:51:53 -0500

    inital commit + day one:

---
+++ day_one/trebuchet.py
@@ -0,0 +1,23 @@
+def get_calibration_value(line: str) -> int:
+    first_digit = last_digit = -1
+    for char in line:
+        if char.isnumeric():
+            if first_digit == -1:  # first digit we see
+                first_digit = char
+                last_digit = char
+            else:
+                last_digit = char
+
+    return int(f"{first_digit}{last_digit}")
+
+
+def sum_calibration_values() -> int:
+    with open("calibration_document.txt") as f:
+        res = 0
+        for line in f:
+            res += get_calibration_value(line)
+        return res
+
+
+if __name__ == "__main__":
+    print(sum_calibration_values())
