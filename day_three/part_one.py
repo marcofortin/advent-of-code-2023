c4e7a3550a56c5c08ac594632bde1280e35293f8
marcofortin <mfortin.uwaterloo@gmail.com>
2023-12-03 17:49:22 -0500

    day3 part 1

---
+++ day_three/part_one.py
@@ -0,0 +1,98 @@
+from typing import List, Set, Tuple
+
+
+def get_adjacent_part_numbers(
+    row_idx: int, col_idx: int, lines: List[str], seen: Set[Tuple[int, int]]
+) -> int:
+    if (
+        row_idx == -1
+        or row_idx == len(lines)
+        or col_idx == -1
+        or col_idx == len(lines[0])
+    ):
+        return 0  # outside grid
+
+    if not lines[row_idx][col_idx].isnumeric():
+        return 0  # not a number
+
+    if (row_idx, col_idx) in seen:
+        return 0  # already accounted for this number
+
+    res = lines[row_idx][col_idx]
+    for i in range(col_idx - 1, -1, -1):  # prefix with digits to left
+        if not lines[row_idx][i].isnumeric():
+            break  # not a number, stop looking left
+
+        if (row_idx, i) in seen:
+            raise Exception(
+                f"Already seen number at coordinates ({row_idx}, {i}). Should have already seen since "
+                f"connected to the digit at "
+                f"coordinate ({row_idx}, {col_idx})."
+            )
+
+        seen.add((row_idx, i))
+        res = (lines[row_idx][i]) + res
+
+    for i in range(col_idx + 1, len(lines[row_idx])):  # postfix with digits to right
+        if not lines[row_idx][i].isnumeric():
+            break  # not a number, stop looking left
+
+        if (row_idx, i) in seen:
+            raise Exception(
+                f"Already seen number at coordinates ({row_idx}, {i}). Should have already seen since "
+                f"connected to the digit at "
+                f"coordinate ({row_idx}, {col_idx})."
+            )
+
+        seen.add((row_idx, i))
+        res += lines[row_idx][i]
+
+    return int(res)
+
+
+def sum_part_numbers() -> int:
+    res = 0
+    seen = set()
+
+    with open("input.txt") as f:
+        lines = [line.rstrip("\n") for line in f.readlines()]
+
+        for row_idx in range(len(lines)):
+            for col_idx in range(len(lines[row_idx])):
+                if (row_idx, col_idx) in seen:
+                    continue
+
+                char = lines[row_idx][col_idx]
+                if char.isnumeric() or char == ".":
+                    continue
+
+                res += get_adjacent_part_numbers(
+                    row_idx - 1, col_idx - 1, lines, seen
+                )  # top left
+                res += get_adjacent_part_numbers(
+                    row_idx - 1, col_idx, lines, seen
+                )  # top
+                res += get_adjacent_part_numbers(
+                    row_idx, col_idx - 1, lines, seen
+                )  # left
+                res += get_adjacent_part_numbers(
+                    row_idx - 1, col_idx + 1, lines, seen
+                )  # top right
+                res += get_adjacent_part_numbers(
+                    row_idx, col_idx + 1, lines, seen
+                )  # right
+                res += get_adjacent_part_numbers(
+                    row_idx + 1, col_idx - 1, lines, seen
+                )  # bottom left
+                res += get_adjacent_part_numbers(
+                    row_idx + 1, col_idx, lines, seen
+                )  # bottom
+                res += get_adjacent_part_numbers(
+                    row_idx + 1, col_idx + 1, lines, seen
+                )  # bottom right
+
+        return res
+
+
+if __name__ == "__main__":
+    print(sum_part_numbers())
